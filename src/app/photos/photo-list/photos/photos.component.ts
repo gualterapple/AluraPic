4b0fa43191617a77705549467231105ee1c657a7
gualter_apple <gualter_apple@hotmail.com>
2021-04-18 11:33:04 +0100

    Capitulo 5 - Novo Componente, Novos Conceitos

---
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Photo } from '../../photo/photo';
+
+@Component({
+  selector: 'app-photos',
+  templateUrl: './photos.component.html',
+  styleUrls: ['./photos.component.css']
+})
+export class PhotosComponent implements OnChanges {
+
+  constructor() { }
+
+  @Input() photos: Photo[] = [];
+  rows: any[] = [];
+
+  ngOnChanges(changes: SimpleChanges) {
+    if(changes.photos)
+    this.rows = this.groupColums(this.photos);
+  }
+  groupColums(photos: Photo[]){
+    const newRows: any[] = [];
+
+    for (let index = 0; index < photos.length; index+=3) {
+
+      debugger;
+      newRows.push(photos.slice(index, index + 3));
+      
+    }
+
+    return newRows;
+  }
+
+
+}
