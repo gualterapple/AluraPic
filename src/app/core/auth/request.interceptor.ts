5ac22803416c6538638206b4713f1ac9a389feb2
gualterapple <39972000+gualterapple@users.noreply.github.com>
2021-07-16 01:35:12 +0100

    Módulo 2 finalizado

---
+++ src/app/core/auth/request.interceptor.ts
@@ -0,0 +1,40 @@
+import { HeaderComponent } from './../header/header.component';
+import { TokenService } from './../token/toke.service';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpHeaderResponse,
+  HttpInterceptor,
+  HttpProgressEvent,
+  HttpRequest,
+  HttpResponse,
+  HttpSentEvent,
+  HttpUserEvent,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class RequestInterceptor implements HttpInterceptor {
+  constructor(private tokenService: TokenService) {}
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<
+    | HttpSentEvent
+    | HttpHeaderResponse
+    | HttpProgressEvent
+    | HttpResponse<any>
+    | HttpUserEvent<any>
+  > {
+    if (this.tokenService.hasToken()) {
+      const token = this.tokenService.getToken();
+      if (token) {
+        req = req.clone({
+          setHeaders: { 'x-access-token': token },
+        });
+      }
+    }
+    return next.handle(req);
+  }
+}
