2741bc632c935d8062aad8ab9ba21ee91162a97c
gualterapple <39972000+gualterapple@users.noreply.github.com>
2021-09-27 04:05:17 +0100

    Removendo rota da history API done

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -40,7 +40,7 @@ export class PhotoDetailsComponent implements OnInit {
       res =>
       {
         this.alertService.success("Photo removed", true);
-        this.router.navigate(['/user', this.userService.getUserName()]);
+        this.router.navigate(['/user', this.userService.getUserName()], {replaceUrl: true});
       },
       error =>
       {
